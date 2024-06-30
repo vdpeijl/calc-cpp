64ff50555e942927ae6f36759d628744dd781ce6
Robbin van der Peijl <robbin@sowiso.com>
2024-06-30 22:48:15 +0200

    Calculator methods testing

--- main.cpp
+++ main.cpp
@@ -29,10 +29,10 @@ int main() {
   double add_result = calculator.add(10, 5);
   double subtract_result = calculator.sub(10, 5);
   double divide_result = calculator.div(10, 3);
-  double multiply_result = calculator.mul(10, 5);
+  double multiplyResult = calculator.mul(10, 5);
 
   std::cout << add_result << std::endl;
   std::cout << subtract_result << std::endl;
   std::cout << divide_result << std::endl;
-  std::cout << multiply_result << std::endl;
+  std::cout << multiplyResult << std::endl;
 }
--- test.cpp
+++ test.cpp
@@ -1,6 +1,34 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "calc.hpp"
+#include "cmd.hpp"
 
-TEST_CASE("Example test") {
-  CHECK(1 + 1 == 2);
+TEST_CASE("Calculator methods") {
+  Command* add = new Command([](const double a, const double b) {
+    return a + b;
+  });
+
+  Command* subtract = new Command([](const double a, const double b) {
+    return a - b;
+  });
+
+  Command* divide = new Command([](const double a, const double b) {
+    return a / b;
+  });
+
+  Command* multiply = new Command([](const double a, const double b) {
+    return a * b;
+  });
+
+  Calculator calculator(
+    add,
+    subtract,
+    divide,
+    multiply
+  );
+
+  CHECK(calculator.add(10, 5) == 15);
+  CHECK(calculator.sub(10, 5) == 5);
+  CHECK(calculator.div(10, 2) == 5);
+  CHECK(calculator.mul(10, 5) == 50);
 }
