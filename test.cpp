719d494d92deeb9a1eca5a4c7914d68488c8e9c5
Robbin van der Peijl <robbin@sowiso.com>
2024-06-30 22:57:10 +0200

    Clean test file

--- test.cpp
+++ test.cpp
@@ -3,23 +3,23 @@
 #include "calc.hpp"
 #include "cmd.hpp"
 
+Command* add = new Command([](const double a, const double b) {
+  return a + b;
+});
+
+Command* subtract = new Command([](const double a, const double b) {
+  return a - b;
+});
+
+Command* divide = new Command([](const double a, const double b) {
+  return a / b;
+});
+
+Command* multiply = new Command([](const double a, const double b) {
+  return a * b;
+});
+
 TEST_CASE("Calculator methods") {
-  Command* add = new Command([](const double a, const double b) {
-    return a + b;
-  });
-
-  Command* subtract = new Command([](const double a, const double b) {
-    return a - b;
-  });
-
-  Command* divide = new Command([](const double a, const double b) {
-    return a / b;
-  });
-
-  Command* multiply = new Command([](const double a, const double b) {
-    return a * b;
-  });
-
   Calculator calculator(
     add,
     subtract,
@@ -29,6 +29,22 @@ TEST_CASE("Calculator methods") {
 
   CHECK(calculator.add(10, 5) == 15);
   CHECK(calculator.sub(10, 5) == 5);
-  CHECK(calculator.div(10, 2) == 5);
+  CHECK(calculator.div(10, 5) == 2);
   CHECK(calculator.mul(10, 5) == 50);
+}
+
+TEST_CASE("Add command") {
+  CHECK(add->execute(10, 5) == 15);
+}
+
+TEST_CASE("Subtract command") {
+  CHECK(subtract->execute(10, 5) == 5);
+}
+
+TEST_CASE("Divide command") {
+  CHECK(divide->execute(10, 5) == 2);
+}
+
+TEST_CASE("Multiply command") {
+  CHECK(multiply->execute(10, 5) == 50);
 }
